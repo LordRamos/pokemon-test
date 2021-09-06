41665b9e44780d9e78a04ce6a5328c99e9f4c5e7
Juan Ramos <lord.ramoss@gmail.com>
2021-09-06 13:57:34 -0400

    feature: query filter and sort

--- pokemon-api/main/controllers/pokemon_controller.py
+++ pokemon-api/main/controllers/pokemon_controller.py
@@ -2,11 +2,26 @@
 from main.schemas.pokemon_schema import PokemonSchema
 from flask import request, Blueprint
 from flask import abort
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from ..schemas.pokemon_schema import PokemonSchema
 from ..models.pokemon import Pokemon
 from marshmallow import ValidationError
 
+parser = reqparse.RequestParser()
+parser.add_argument('number', type=int)
+parser.add_argument('type1', type=str)
+parser.add_argument('type2', type=str)
+parser.add_argument('total', type=int)
+parser.add_argument('hp', type=int)
+parser.add_argument('attack', type=int)
+parser.add_argument('defense', type=int)
+parser.add_argument('sp_atk', type=int)
+parser.add_argument('sp_def', type=int)
+parser.add_argument('speed', type=int)
+parser.add_argument('generation', type=int)
+parser.add_argument('legendary', type=bool)
+parser.add_argument('order', type=str)
+parser.add_argument('sort_by', type=str)
 pokemon_schema = PokemonSchema()
 pokemon_v1_bp = Blueprint('pokemon_v1_bp', __name__)
 api = Api(pokemon_v1_bp)
@@ -14,8 +29,21 @@ api = Api(pokemon_v1_bp)
 
 class PokemonListResource(Resource):
     def get(self):
-        pokemons = Pokemon.get_all()
-        result = pokemon_schema.dump(pokemons, many=True)
+        q_params = {k: v for k, v in parser.parse_args().items()
+                    if v is not None}
+        order = q_params.pop("order")if "order" in q_params.keys() else None
+        sort_by = q_params.pop(
+            "sort_by")if "sort_by" in q_params.keys() else None
+        pokemons = Pokemon.get_queryset()
+        # filter
+        if(q_params):
+            pokemons = Pokemon.filter(pokemons, **q_params)
+        # sort
+        if order and sort_by:
+            pokemons = Pokemon.order_by(
+                order, sort_by, pokemons)
+
+        result = pokemon_schema.dump(pokemons.all(), many=True)
         return result
 
     def post(self):
--- pokemon-api/main/models/base_model_mixin.py
+++ pokemon-api/main/models/base_model_mixin.py
@@ -1,4 +1,5 @@
 from main.ext import db
+from sqlalchemy import desc, asc
 
 
 class BaseModelMixin:
@@ -15,9 +16,22 @@ class BaseModelMixin:
         return cls.query.all()
 
     @classmethod
+    def get_queryset(cls):
+        return cls.query
+
+    @classmethod
     def get(cls, id):
         return cls.query.get(id)
 
     @classmethod
-    def filter(cls, **kwargs):
-        return cls.query.filter_by(**kwargs).all()
+    def filter(cls, queryset=None, **kwargs):
+        queryset = queryset if queryset else cls.query
+        return queryset.filter_by(**kwargs)
+
+    @classmethod
+    def order_by(cls, order_type, field_name, queryset=None):
+        queryset = queryset if queryset else cls.query
+        if order_type == "desc":
+            return queryset.order_by(desc(getattr(cls, field_name)))
+        else:
+            return queryset.order_by(asc(getattr(cls, field_name)))
